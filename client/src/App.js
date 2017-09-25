a998646a2cd34b01698492a70d0a1e07345a5d47
MikeNewm32 <michaelnewman85@hotmail.com>
2017-09-24 23:57:28 -0400

    Added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -11,22 +11,26 @@ import EditFrog from './components/EditFrog';
 import List from './components/List';
 import ListItem from './components/ListItem';
 import EditList from './components/EditList';
+import Navbar from './components/Navbar';
 
 class App extends Component {
   render(){
     return (
       <div>
       <Router>
-          <div>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/user/:userId/" component={UserHome} />
-            <Route exact path="/user/:userId/lists" component={Lists} />
-            <Route exact path="/user/:userId/lists/new" component={NewList} />
-            <Route exact path="/user/:userId/editlist/:listId" component={EditList} />
-            <Route exact path="/user/:userId/lists/:listId/" component={ListItem} />
-            <Route exact path="/user/:userId/lists/:listId/frogs/new/" component={NewFrog}/>
-            <Route exact path="/user/:userId/lists/:listId/frogs/:frogId/edit/" component={EditFrog}/>
-          </div>
+        <div>
+          <Navbar />
+            <div>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/user/:userId/" component={UserHome} />
+              <Route exact path="/user/:userId/lists" component={Lists} />
+              <Route exact path="/user/:userId/lists/new" component={NewList} />
+              <Route exact path="/user/:userId/editlist/:listId" component={EditList} />
+              <Route exact path="/user/:userId/lists/:listId/" component={ListItem} />
+              <Route exact path="/user/:userId/lists/:listId/frogs/new/" component={NewFrog}/>
+              <Route exact path="/user/:userId/lists/:listId/frogs/:frogId/edit/" component={EditFrog}/>
+            </div>
+        </div>
       </Router>
       </div>
     );
